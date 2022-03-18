93b11f91fa536ca90bee522be08daf66fb365418
Ketosvaldo <ketchulo.conk2013@gmail.com>
2022-03-18 13:35:26 -0600

    xd

--- guessmynumber/src/components/Game/Game.js
+++ guessmynumber/src/components/Game/Game.js
@@ -4,17 +4,41 @@ class Game extends Component {
     constructor(){
         super()
         this.state = {
-            number: 0,
-            message: "",
+            number: "",
+            message: "Hola",
             random: generateRandomNumber(100)
         }
     }
+
+    handleOnChange = e => {
+        const {target: {value}} = e;
+        console.log(value);
+        if(value.trim() > 0){
+            this.setState({
+                number: value,
+            });
+        }
+    }
+
+    handleOnClick = () => {
+        const number = parseInt(this.state.number);
+        const random = parseInt(this.state.random);
+        const text = calculateText(number, random);
+        this.setState({
+            message: text,
+        })
+    }
     render() {
+        console.log(this.state.random);
         return (
             <div className="Game">
-                <input type="number"/>
-                <button>Probar</button>
-                <p>Texto</p>
+                <input 
+                    type="number"
+                    value = {this.state.number}
+                    onChange = {this.handleOnChange}
+                />
+                <button onClick={this.handleOnClick}>Probar</button>
+                <p>{this.state.message}</p>
             </div>
         );
     }
@@ -24,4 +48,10 @@ export default Game;
 
 function generateRandomNumber(max, min = 1){
     return Math.floor(Math.random() * (max-min) + min);
+}
+
+function calculateText(number, random){
+    if(number === random){
+        return "Felicidades, has acertado"
+    }
 }
